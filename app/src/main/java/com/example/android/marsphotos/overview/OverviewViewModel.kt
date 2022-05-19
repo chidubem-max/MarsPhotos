9306c909fd1d5fa2d77f351f5bb0fab666e40db7
Onyeukwu Chidubem <onyeukwuchidubemclinton@gmail.com>
2022-05-19 02:09:10 +0100

    Learning about HTTPS and RESTful Services and API using Retrofit and Corountines

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -21,7 +21,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.marsphotos.network.MarsApi
+import com.example.android.marsphotos.network.MarsPhoto
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -33,6 +35,10 @@ class OverviewViewModel : ViewModel() {
 
     // The external immutable LiveData for the request status
     val status: LiveData<String> = _status
+
+    private val _photos = MutableLiveData<List<MarsPhoto>>()
+
+    val photos : LiveData<List<MarsPhoto>> = _photos
     /**
      * Call getMarsPhotos() on init so we can display status immediately.
      */
@@ -46,8 +52,12 @@ class OverviewViewModel : ViewModel() {
      */
     private fun getMarsPhotos() {
         viewModelScope.launch {
-            val listResult = MarsApi.retrofitService.getPhotos()
-            _status.value = listResult
+            try {
+                _photos.value = MarsApi.retrofitService.getPhotos()
+                _status.value = "Success: Mars properties retrieved"
+            } catch (e: Exception){
+                _status.value = "Failure: ${e.message}"
+            }
         }
     }
 }
